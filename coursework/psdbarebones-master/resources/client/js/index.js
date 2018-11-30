1f14a17c36ad896c0196a575f090fc7ebb84376e
JoshuaTuck <40886114+JoshuaTuck@users.noreply.github.com>
2018-11-30 12:29:57 +0000

    Add files via upload

--- coursework/psdbarebones-master/resources/client/js/index.js
+++ coursework/psdbarebones-master/resources/client/js/index.js
@@ -1,3 +1,36 @@
 function pageLoad() {
+    $.ajax({
+        url: '/thing/list',
+        type: 'GET',
+        success: thingList => {
+
+            let thingsHTML = `<div class="container">`
+                + `<div class="row mb-2">`
+                + `<div class="col-2 bg-light font-weight-bold">One</div>`
+                + `<div class="col-2 bg-light font-weight-bold">Two</div>`
+                + `<div class="col-2 bg-light font-weight-bold">Three</div>`
+                + `<div class="col-2 bg-light font-weight-bold">Four</div>`
+                + `<div class="col-2 bg-light font-weight-bold">Five</div>`
+                + `<div class="col-2 bg-light font-weight-bold">Six</div>`
+                + `</div>`;
+
+            for (let thing of thingList) {
+                thingsHTML += `<div class="row mb-2">`
+                    + `<div class="col-2">${thing.one}</div>`
+                    + `<div class="col-2">${thing.two}</div>`
+                    + `<div class="col-2">${thing.three}</div>`
+                    + `<div class="col-2">${thing.four}</div>`
+                    + `<div class="col-2">${thing.five}</div>`
+                    + `<div class="col-2">${thing.six}</div>`
+                    + `</div>`;
+            }
+            thingsHTML += `</div>`;
+
+            $('#stuff').html(thingsHTML);
+        }
+
+    });
+
+}
 
 }
